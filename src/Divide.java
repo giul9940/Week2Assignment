3619e8067201ca5328f213c912a00d0ef4ffbb14
giul9940 <giul9940@ISS2V0SKH2.scdsb.pri>
2019-09-18 13:15:16 -0400

    Divide

--- src/Divide.java
+++ src/Divide.java
@@ -4,6 +4,25 @@ public class Divide {
     
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int x, y, z = 0, r;
+        System.out.print("Enter Number 1:");
+        x = s.nextInt();
+        System.out.println("Enter Number 2:");
+        y = s.nextInt();
+        
+        if(x < y){
+            z = x % y;
+            r = z / 3;
+            System.out.println(x + " divided by " + y + " is " + z+"r"+r);
+        }
+        if(y < x){
+            z = y % x;
+            r = z / 3;
+            System.out.println(y + " divided by " + x + " is " + z);
+        }
+        
+        
+        
     }
     
 }
