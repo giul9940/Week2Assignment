4da39fc80622e258f9e318852a214ca7f2825e94
giul9940 <giul9940@ISS2V0SKH2.scdsb.pri>
2019-09-19 12:53:10 -0400

    PowersChart

--- src/PowersChart.java
+++ src/PowersChart.java
@@ -4,6 +4,18 @@ public class PowersChart {
    
     public static void main(String[] args) {
        Scanner s = new Scanner(System.in);
+       int x, y;
+       while(true){
+            System.out.print("Enter a value from 1 to 10: ");
+            x = s.nextInt();
+            if(x < 0 || x > 10) System.out.println("Error - 0 to 10 numbers only.");
+            else break;
+       }        
+        System.out.println("X\t\t2^X");
+        for (int i = 0; i <= x; i++) {            
+            y = (int) Math.pow(2, i);
+            System.out.println(i +"\t\t"+"2^"+i+" = "+y);           
+        }
     }
     
 }
