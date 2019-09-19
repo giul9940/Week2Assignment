b9cdda7b3c946e0a1d75f9d44826876513cc1819
giul9940 <giul9940@ISS2V0SKH2.scdsb.pri>
2019-09-19 13:12:25 -0400

    SalaryTable

--- src/SalaryTable.java
+++ src/SalaryTable.java
@@ -1,8 +1,19 @@
 
 public class SalaryTable {
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {          
+        int currentsalary = 40000;               
+        int newsalary;
+        System.out.println("YEAR\t\tOLD SALARY\t\tRAISE\t\tNEW SALARY");
+        System.out.println("----\t\t----------\t\t-----\t\t----------");
+        for (int year = 1; year <= 10; year++) {            
+            System.out.format("%d%20d", year, currentsalary);
+            newsalary = (int) (0.03 * currentsalary);
+            currentsalary+=newsalary;
+            System.out.format("%2d%16d\n", newsalary, currentsalary); 
+        }
+               
     }
     
 }
+//year+"\t\t"+currentsalary+"\t\t\t"+raiseincrease+"\t\t"+newsalary
