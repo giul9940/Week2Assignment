aec70e515f93f1f284c6ce25de80225285bc57f2
giul9940 <giul9940@ISS2V0SKH2.scdsb.pri>
2019-09-19 12:33:15 -0400

    Divide

--- src/Divide.java
+++ src/Divide.java
@@ -1,28 +1,40 @@
+
 import java.util.Scanner;
 
 public class Divide {
-    
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int x, y, z = 0, r;
-        System.out.print("Enter Number 1:");
-        x = s.nextInt();
-        System.out.println("Enter Number 2:");
-        y = s.nextInt();
-        
-        if(x > y){
+        int x = 0, y = 0, z = 0, r;
+        while(true){
+        try {            
+            System.out.print("Enter Number 1: ");
+            x = s.nextInt();
+            System.out.print("Enter Number 2: ");
+            y = s.nextInt();
+        } catch (Exception e) {
+            System.out.println("Thats not a number! Try again.");
+            s.nextLine();
+            continue;
+        }
+        break;
+        }
+        if(x == y) {
             z = x % y;
             r = x / y;
-            System.out.println(x + " divided by " + y + " is " + r+"r"+z);
+            System.out.println(x + " divided by " + y + " is " + r + "r" + z);
         }
-        if(y > x){
+        if (x > y) {
+            z = x % y;
+            r = x / y;
+            System.out.println(x + " divided by " + y + " is " + r + "r" + z);
+        }
+        if (y > x) {
             z = y % x;
             r = y / x;
-            System.out.println(y + " divided by " + x + " is " + r+"r"+z);
+            System.out.println(y + " divided by " + x + " is " + r + "r" + z);
         }
-        
-        
-        
+
     }
-    
+
 }
